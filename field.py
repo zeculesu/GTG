ce4bb7a1024319a3ae9acff871911a2eec2698bb
arti <artiyjana@yandex.ru>
2020-12-24 10:05:43 +0300

    refactor hero

--- field.py
+++ field.py
@@ -1,16 +1,12 @@
 from random import choice
 from cell import *
-
+from Hero import *
 
 # import pygame.examples.eventlist
 # pygame.examples.eventlist.main()
 
 
 class Field(pg.sprite.Sprite):
-    hero = pg.image.load('itachi.jpg')
-    hero = pg.transform.scale(hero, (60, 60))
-    hero.set_colorkey((255, 255, 255))
-
     def __init__(self, screen_size: tuple, screen, group: pg.sprite.AbstractGroup):
         super(Field, self).__init__(group)
         screen_width, screen_height = screen_size
@@ -20,6 +16,12 @@ class Field(pg.sprite.Sprite):
         self.rect.y = int(screen_height * 0.1)
         self.cells = [[None] * 12 for _ in range(12)]
         self.screen = screen
+        self.cell_size = 60
+        self.distribution_of_cells()
+        self.current_cell = [0, 0]
+        # print(self.cells)
+
+    def distribution_of_cells(self) -> None:
         options = {Cell: [0, 78],
                    Trap: [0, 60],
                    Health: [0, 40],
@@ -39,16 +41,8 @@ class Field(pg.sprite.Sprite):
                 self.cells[i][j] = option
                 if options[option][1] <= options[option][0]:
                     del options[option]
-        self.current_cell = [0, 0]
-        # print(self.cells)
-        self.move_hero()
 
-    def move_hero(self):
-        Field.hero_rect = Field.hero.get_rect(
-            bottomright=(60 * (self.current_cell[0] + 1), 60 * (self.current_cell[1] + 1)))
-        self.screen.blit(Field.hero, Field.hero_rect)
-
-    def get_sibling_cells(self, i, j):
+    def get_sibling_cells(self, i, j) -> list:
         cells = ['self.cells[i - 1][j - 1]', 'self.cells[i - 1][j]', 'self.cells[i - 1][j + 1]',
                  'self.cells[i][j - 1]', 'self.cells[i][j + 1]',
                  'self.cells[i + 1][j - 1]', 'self.cells[i + 1][j]', 'self.cells[i + 1][j + 1]']
@@ -60,50 +54,55 @@ class Field(pg.sprite.Sprite):
                 continue
         return square
 
-    def handle_move(self, event: pg.event.Event):
+    def handle_move(self, event: pg.event.Event) -> None:
         i, j = self.current_cell
         if event.key == pg.K_UP or event.key == pg.K_w:
             if self.current_cell[1] != 0:
-                self.cells[i][j] = 'way'
+                self.be_way(i, j)
                 self.current_cell[1] -= 1
         elif event.key == pg.K_DOWN or event.key == pg.K_s:
             if self.current_cell[1] != 11:
-                self.cells[i][j] = 'way'
+                self.be_way(i, j)
                 self.current_cell[1] += 1
         elif event.key == pg.K_LEFT or event.key == pg.K_a:
             if self.current_cell[0] != 0:
-                self.cells[i][j] = 'way'
+                self.be_way(i, j)
                 self.current_cell[0] -= 1
         elif event.key == pg.K_RIGHT or event.key == pg.K_d:
             if self.current_cell[0] != 11:
-                self.cells[i][j] = 'way'
+                self.be_way(i, j)
                 self.current_cell[0] += 1
+
+    def be_way(self, i, j) -> None:
+        self.cells[i][j] = 'way'
 
     def render(self, screen):
         screen.fill('black')
-        cell_size = 60
         for i in range(12):
             for j in range(12):
                 if str(self.cells[i][j]) == "finish":
-                    screen.fill(pg.Color('red'), (cell_size * i, cell_size * j,
-                                                  cell_size, cell_size))
+                    screen.fill(pg.Color('red'), (self.cell_size * i, self.cell_size * j,
+                                                  self.cell_size, self.cell_size))
                 if str(self.cells[i][j]) == "way":
-                    screen.fill(pg.Color('lightgreen'), (cell_size * i, cell_size * j,
-                                                  cell_size, cell_size))
+                    screen.fill(pg.Color('lightgreen'), (self.cell_size * i, self.cell_size * j,
+                                                         self.cell_size, self.cell_size))
                 # elif str(self.cells[i][j]) == "<class 'cell.Task'>":
-                #     screen.fill(pg.Color('#536dfe'), (cell_size * i, cell_size * j,
-                #                                   cell_size, cell_size))
+                #     screen.fill(pg.Color('pink'), (self.cell_size * i, self.cell_size * j,
+                #                                   self.cell_size, self.cell_size))
                 # elif str(self.cells[i][j]) == "<class 'cell.Health'>":
-                #     screen.fill(pg.Color('#00e676'), (cell_size * i, cell_size * j,
-                #                                     cell_size, cell_size))
+                #     screen.fill(pg.Color('green'), (self.cell_size * i, self.cell_size * j,
+                #                                     self.cell_size, self.cell_size))
                 # elif str(self.cells[i][j]) == "<class 'cell.Trap'>":
-                #     screen.fill(pg.Color('#ff9100'), (cell_size * i, cell_size * j,
-                #                                      cell_size, cell_size))
+                #     screen.fill(pg.Color('orange'), (self.cell_size * i, self.cell_size * j,
+                #                                      self.cell_size, self.cell_size))
                 # elif str(self.cells[i][j]) == "<class 'cell.Teleport'>":
-                #     screen.fill(pg.Color('#9d46ff'), (cell_size * i, cell_size * j,
-                #                                      cell_size, cell_size))
-                pg.draw.rect(screen, 'white', (cell_size * i, cell_size * j, cell_size, cell_size), 2)
+                #     screen.fill(pg.Color('purple'), (self.cell_size * i, self.cell_size * j,
+                #                                      self.cell_size, self.cell_size))
+                pg.draw.rect(screen, 'white', (self.cell_size * i, self.cell_size * j,
+                                               self.cell_size, self.cell_size), 2)
 
+    def get_current_cell(self) -> list:
+        return self.current_cell
 
 def main():
     pg.init()
@@ -112,6 +111,7 @@ def main():
     pg.display.set_caption('Goof the Game')
     all_sprites = pg.sprite.Group()
     field = Field(size, screen, all_sprites)
+    hero = Hero(screen, all_sprites)
     running = True
     while running:
         for event in pg.event.get():
@@ -120,7 +120,7 @@ def main():
             elif event.type == pg.KEYDOWN:
                 field.handle_move(event)
             field.render(screen)
-            field.move_hero()
+            hero.move_hero(field.get_current_cell())
         pg.display.flip()
     pg.quit()
 
