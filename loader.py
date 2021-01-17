b43898155bd47b2b878096ce8ce5b82f72b710c2
arti <artiyjana@yandex.ru>
2021-01-17 14:03:21 +0300

    added comments

--- loader.py
+++ loader.py
@@ -1,13 +1,14 @@
 import os
 import pygame as pg
+
 from exceptions import ImageNotFoundError, FontNotFoundError, SoundNotFoundError
 
 
-class Loader:
+class Loader:  # загрузка файлов
     pg.mixer.init()
 
     @staticmethod
-    def load_image(filename: str) -> pg.Surface:
+    def load_image(filename: str) -> pg.Surface:  # загрузка картинок
         try:
             image = pg.image.load(os.path.join('data', 'img', filename))
         except FileNotFoundError:
@@ -15,7 +16,7 @@ class Loader:
         return image
 
     @staticmethod
-    def load_font(fontname: str, size: int) -> pg.font.Font:
+    def load_font(fontname: str, size: int) -> pg.font.Font:  # загрузка шрифта
         try:
             font = pg.font.Font(os.path.join('data', 'font', fontname), size)
         except FileNotFoundError:
@@ -23,7 +24,7 @@ class Loader:
         return font
 
     @staticmethod
-    def load_sound(filename: str) -> pg.mixer.Sound:
+    def load_sound(filename: str) -> pg.mixer.Sound:  # загрузка музычки
         try:
             sound = pg.mixer.Sound(os.path.join('data', 'music', filename))
         except FileNotFoundError:
@@ -31,8 +32,7 @@ class Loader:
         return sound
 
     @staticmethod
-    def load_level(filename: str) -> list:
-        filename = "data/map/" + filename
-        with open(filename, 'r') as mapFile:
+    def load_level(filename: str) -> list:  # загрузка уровня
+        with open(os.path.join('data', 'map', filename), 'r') as mapFile:
             level_map = [line.strip() for line in mapFile]
         return list(level_map)
